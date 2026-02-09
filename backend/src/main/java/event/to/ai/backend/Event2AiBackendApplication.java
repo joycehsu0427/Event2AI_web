6554199aaeb6012c468cd69392f2d5189ae7de74
joyce <joycehsu427@gmail.com>
2026-02-09 21:48:22 +0800

    initial commit

---
+++ backend/src/main/java/event/to/ai/backend/Event2AiBackendApplication.java
@@ -0,0 +1,13 @@
+package event.to.ai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Event2AiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Event2AiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/event/to/ai/backend/Event2AiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package event.to.ai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Event2AiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
