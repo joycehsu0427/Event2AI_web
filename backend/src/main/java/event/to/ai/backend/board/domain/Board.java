b8ce483b5a3f2ecff5806f9da974d54f376f087e
Du-Brandon <asdffhnkl123@gmail.com>
2026-03-06 19:03:43 +0800

    [Feature addition] Finish event

--- backend/src/main/java/event/to/ai/backend/board/domain/Board.java
+++ backend/src/main/java/event/to/ai/backend/board/domain/Board.java
@@ -3,7 +3,10 @@ package event.to.ai.backend.board.domain;
 import tw.teddysoft.ezddd.entity.EsAggregateRoot;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.UUID;
+
+import static tw.teddysoft.ucontract.Contract.ensureNotNull;
 
 public class Board extends EsAggregateRoot<BoardId, BoardEvent> {
     private BoardId boardId;
@@ -14,21 +17,43 @@ public class Board extends EsAggregateRoot<BoardId, BoardEvent> {
     private LocalDateTime updatedAt;
 
     @Override
-    protected void when(BoardEvent e){
-
+    protected void when(BoardEvent boardEvent) {
+        switch (boardEvent) {
+            case BoardEvent.BoardCreated e -> {
+                this.boardId = e.boardId();
+                this.boardTitle = e.title();
+                this.description = e.description();
+                this.ownerId = e.ownerId();
+                this.isDeleted = false;
+                this.createdAt = LocalDateTime.ofInstant(e.occurredOn(), ZoneOffset.UTC);
+                this.updatedAt = this.createdAt;
+            }
+            case BoardEvent.BoardDeleted e -> {
+                this.isDeleted = true;
+                this.updatedAt = LocalDateTime.ofInstant(e.occurredOn(), ZoneOffset.UTC);
+            }
+            case BoardEvent.BoardRenamed e -> {
+                this.boardTitle = e.title();
+                this.updatedAt = LocalDateTime.ofInstant(e.occurredOn(), ZoneOffset.UTC);
+            }
+        }
     }
 
     @Override
-    protected void ensureInvariant(){
-
+    protected void ensureInvariant() {
+        ensureNotNull("boardId", boardId);
+        ensureNotNull("boardTitle", boardTitle);
+        ensureNotNull("description", description);
+        ensureNotNull("ownerId", ownerId);
     }
 
     @Override
-    public BoardId getId(){
+    public BoardId getId() {
         return boardId;
     }
 
-    @Override public String getCategory() {
+    @Override
+    public String getCategory() {
         return "board";
     }
 
@@ -38,11 +63,13 @@ public class Board extends EsAggregateRoot<BoardId, BoardEvent> {
         return board;
     }
 
-    public void rename(String title){
-//        apply(new BoardRenamed(...));
+    public void rename(BoardTitle title) {
+        if (this.isDeleted == false){
+            apply(new BoardEvent.BoardRenamed(boardId, title));
+        }
     }
 
-    public void delete(){
-
+    public void delete() {
+        apply(new BoardEvent.BoardDeleted(boardId));
     }
 }
--- backend/src/main/java/event/to/ai/backend/board/domain/BoardEvent.java
+++ backend/src/main/java/event/to/ai/backend/board/domain/BoardEvent.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 public sealed interface BoardEvent extends InternalDomainEvent
         permits BoardEvent.BoardCreated, BoardEvent.BoardRenamed, BoardEvent.BoardDeleted {
 
-
     record BoardCreated(
             UUID id,
             Instant occurredOn,
@@ -19,12 +18,36 @@ public sealed interface BoardEvent extends InternalDomainEvent
             UUID ownerId,
             BoardTitle title,
             String description
-    ) implements BoardEvent {
+    ) implements BoardEvent, InternalDomainEvent.ConstructionEvent {
 
-        public BoardCreated(BoardId id, BoardTitle title, String description, UUID ownerId) {
-            this(UUID.randomUUID(), Instant.now(), id.toString(), Map.of(), id, ownerId, title, description);
+        public BoardCreated(BoardId boardId, BoardTitle title, String description, UUID ownerId) {
+            this(UUID.randomUUID(), Instant.now(), boardId.toString(), Map.of(), boardId, ownerId, title, description);
         }
     }
 
+    record BoardRenamed(
+            UUID id,
+            Instant occurredOn,
+            String source,
+            Map<String, String> metadata,
+            BoardId boardId,
+            BoardTitle title
+    ) implements BoardEvent {
+        public BoardRenamed(BoardId boardId, BoardTitle title) {
+            this(UUID.randomUUID(), Instant.now(), boardId.toString(), Map.of(), boardId, title);
+        }
+    }
 
+    record BoardDeleted(
+            UUID id,
+            Instant occurredOn,
+            String source,
+            Map<String, String> metadata,
+            BoardId boardId
+    ) implements BoardEvent, InternalDomainEvent.DestructionEvent{
+
+        public BoardDeleted(BoardId boardId) {
+            this(UUID.randomUUID(), Instant.now(), boardId.toString(), Map.of(), boardId);
+        }
+    }
 }
